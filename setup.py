3dbf4fe24033ede9edaeff0c4cc796a0276dc872
Bard-Gaming <christophe@dronne.fr>
2023-07-19 21:18:39 +0200

    updated gitignore

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.2'
+DESCRIPTION = 'Minecraft Script Programming language'
+
+# Setting up
+setup(
+    name="minecraft_script",
+    version=VERSION,
+    author="Joyful-Bard",
+    author_email="<thisis@notarealemail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['ply'],
+    keywords=['minecraft', 'mc', 'script', 'language'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
