693ffa4829dafaf044a3a320704be3cc91fb8fcb
Bard-Gaming <christophe@dronne.fr>
2023-07-20 17:44:52 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.3'
+VERSION = '0.1.401'
 DESCRIPTION = 'Minecraft Script Programming language'
 
 # Setting up
@@ -20,7 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['ply'],
+    install_requires=[],
     keywords=['minecraft', 'mc', 'script', 'language'],
     classifiers=[
         "Intended Audience :: Developers",
