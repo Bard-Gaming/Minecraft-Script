e76dbb147165e9a61bb136f278f61a8b96fe952a
Bard-Gaming <christophe@dronne.fr>
2024-01-25 23:02:14 +0100

    Started reworking parser

--- minecraft_script/__main__.py
+++ minecraft_script/__main__.py
@@ -3,13 +3,15 @@ from .shell_commands import shell_functions
 from sys import argv
 from os import getcwd
 
-arguments = argv[1:]
+arguments = argv[1:]  # argv[0] is path of file executing __main__
+
 if not arguments:
     print(f"Minecraft Script version {version}; currently in {getcwd()}\nUse \"help\" for more information.")
     exit()
 
-function = shell_functions.get(arguments[0])
+function_name = arguments.pop(0)
+function = shell_functions.get(function_name)
 if function is not None:
-    function(*arguments[1:])
+    function(*arguments)
 else:
-    print(f'Unknown MCS command: "{arguments[0]}"')
+    print(f'Unknown MCS command: "{function_name}"')
--- minecraft_script/errors.py
+++ minecraft_script/errors.py
@@ -1,32 +1,6 @@
-from .text_additions import text_error, text_underline
-
-
-class MCSError:
-    def __init__(self, details: str, error_type: str = "Unknown Error", line_number: int = None):
-        self.details = details
-        self.error_type = error_type
-        self.line_number = line_number
-
-        print(self)
-
-    def __str__(self):
-        if self.line_number:
-            return text_error(f'{self.error_type}: {self.details} at line {self.line_number}')
-        else:
-            return text_error(f'{self.error_type}: {self.details}')
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}({self.error_type !r}, {self.details !r}, {self.line_number !r})'
-
-
-class MCSSyntaxError(MCSError):
-    def __init__(self, details: str, line_number: int = None):
-        super().__init__(details, "Syntax Error", line_number)
-
-
-class MCSValueError(MCSError):
-    def __init__(self, details: str, line_number: int = None):
-        super().__init__(details, "Value Error", line_number)
+class MCSSyntaxError(SyntaxError):
+    def __init__(self, details: str):
+        super().__init__(details)
 
 
 class MCSIllegalCharacterError(Exception):
@@ -34,31 +8,21 @@ class MCSIllegalCharacterError(Exception):
         super().__init__(f"Illegal Character: {value} (line {position[1]}, {position[0]})")
 
 
-class MCSTypeError(MCSError):
-    def __init__(self, details: str, line_number: int = None):
-        super().__init__(details, "Type Error", line_number)
+class MCSTypeError(TypeError):
+    pass
 
 
-class MCSZeroDivisionError(MCSError):
-    def __init__(self, details: str, line_number: int = None):
-        super().__init__(details, "Zero Division Error", line_number)
+class MCSZeroDivisionError(ZeroDivisionError):
+    pass
 
 
-class MCSNameError(MCSError):
-    def __init__(self, details: str, line_number: int = None):
-        details = f'Name {text_underline(f"{details !r}")} is not defined'
-        super().__init__(details, "Name Error", line_number)
+class MCSValueError(ValueError):
+    pass
 
 
-class MCSIndexError(MCSError):
-    def __init__(self, details: str, *, type = None, line_number: int = None):
-        if type:
-            details = f'{type} index {text_underline(f"{details}")} out of range'
-        else:
-            details = f'Index {text_underline(f"{details}")} out of range'
-        super().__init__(details, "Index Error", line_number)
+class MCSIndexError(IndexError):
+    pass
 
 
-if __name__ == '__main__':
-    a = MCSTypeError('const')
-    print(a)
+class MCSNameError(NameError):
+    pass
