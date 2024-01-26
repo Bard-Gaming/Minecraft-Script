95bedf08a8851763446bb95f4473dd2f3b39f385
Bard-Gaming <christophe@dronne.fr>
2024-01-26 18:20:03 +0100

    finished with basic layout of parser rework

--- minecraft_script/nodes.py
+++ minecraft_script/nodes.py
@@ -10,7 +10,7 @@ class _InterpreterNode:
         return self.value
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({self.value})'
+        return f'{self.__class__.__name__}({self.value !r})'
 
 
 class NumberNode(_InterpreterNode):
@@ -18,6 +18,35 @@ class NumberNode(_InterpreterNode):
         super().__init__(token, token.get_position())
 
 
+class StringNode(_InterpreterNode):
+    def __init__(self, token):
+        super().__init__(token, token.get_position())
+
+
+class ListNode(_InterpreterNode):
+    def __init__(self, node_list, position):
+        super().__init__(node_list, position)
+
+
 class VariableAccessNode(_InterpreterNode):
     def __init__(self, token):
         super().__init__(token, token.get_position())
+
+
+class BinaryOperationNode(_InterpreterNode):
+    def __init__(self, left_value, operator, right_value):
+        self.left_value = left_value  # is Node
+        self.operator: Token = operator
+        self.right_value = right_value  # is Node
+
+        super().__init__(self.operator, self.operator.get_position())
+
+    def get_left_node(self):
+        return self.left_value
+
+    def get_right_node(self):
+        return self.right_value
+
+    def __repr__(self) -> str:
+        return f'BinaryOperationNode({self.left_value !r}, {self.operator !r}, {self.right_value !r})'
+
--- minecraft_script/parser.py
+++ minecraft_script/parser.py
@@ -1,23 +1,30 @@
 from .tokens import Token
+from .errors import *
 from .nodes import *
 
 
 class Parser:
     def __init__(self, token_input: tuple[Token, ...]):
         self.token_input = token_input
-        self.current_token = None
+        self.__current_token: Token = None  # NOQA (Only None until the self.advance() call)
         self.current_index = -1
+
+        self.advance()  # initialize token & index
+
+    @property
+    def current_token(self) -> Token:
+        return self.__current_token if self.__current_token is not None else Token("", "NONE")
 
     def advance(self) -> None:
         if self.current_index >= len(self.token_input) - 1:
-            self.current_token = None
+            self.__current_token = None
             return
 
         self.current_index += 1
-        self.current_token = self.token_input[self.current_index]
+        self.__current_token = self.token_input[self.current_index]
 
     def parse(self) -> tuple:
-        return ()
+        return self.program()
 
     # --------------- Grammar --------------- :
 
@@ -27,15 +34,93 @@ class Parser:
             self.advance()
             return node
 
+        elif self.current_token.tt_type == 'TT_STRING':
+            node = StringNode(self.current_token)
+            self.advance()
+            return node
+
+        elif self.current_token.tt_type == 'TT_BRACKET':
+            return self.make_list()
+
         elif self.current_token.tt_type == 'TT_NAME':
             node = VariableAccessNode(self.current_token)
             self.advance()
             return node
+
+        elif self.current_token.tt_type == 'TT_PARENTHESIS':
+            if self.current_token.value != '(':
+                position = self.current_token.get_position()
+                raise MCSSyntaxError(f"Unmatched closing parenthesis at line {position[1]}, {position[0]}")
+            self.advance()  # skip opening parenthesis
+
+            expression = self.expression()
+
+            if self.current_token.value != ')':
+                position = self.current_token.get_position()
+                raise MCSSyntaxError(f"Unclosed parenthesis at line {position[1]}, {position[0]}")
+            self.advance()  # skip closing parenthesis
+
+            return expression
 
     def atom(self, atom=None):
         atom = self.sub_atom() if atom is None else atom
 
         return atom
 
+    def factor(self):
+        return self.binary_operation(self.atom, ['*', '/', '%'])
+
+    def term(self):
+        return self.binary_operation(self.factor, ['+', '-'])
+
+    def expression(self):
+        return self.term()
+
+    def statement(self):
+        return self.expression()
+
+    def program(self):
+        pass
+
     # --------------- Implementation  --------------- :
 
+    def binary_operation(self, operation_function, operators):
+        left = operation_function()
+
+        if self.current_token.value in operators:
+            operator = self.current_token
+            right = operation_function()
+
+            left = BinaryOperationNode(left, operator, right)
+
+        return left
+
+    def make_list(self) -> ListNode:
+        pos = self.current_token.get_position()  # use starting position for whole node
+        list_nodes = []
+
+        if self.current_token.value != '[':  # has to be a bracket (otherwise function can't be called)
+            raise MCSSyntaxError(f"Expected '[', got {self.current_token.value !r} instead (line {pos[1]}, {pos[0]}")
+        self.advance()
+
+        if self.current_token.tt_type == 'TT_BRACKET' and self.current_token.value == ']':
+            self.advance()
+            return ListNode(list_nodes, pos)
+
+        list_nodes.append(self.expression())
+
+        while self.current_token is not None and self.current_token.tt_type == 'TT_COMMA':  # check if null
+            self.advance()  # skip comma
+
+            if self.current_token.tt_type == 'TT_BRACKET' and self.current_token.value == ']':
+                self.advance()
+                return ListNode(list_nodes, pos)
+
+            list_nodes.append(self.expression())
+
+        if self.current_token.tt_type != 'TT_BRACKET' or self.current_token.value != ']':
+            current_pos = self.current_token.get_position()
+            raise MCSSyntaxError(f'Unclosed list bracket at line {current_pos[1]}, {current_pos[0]}')
+        self.advance()
+
+        return ListNode(list_nodes, pos)
