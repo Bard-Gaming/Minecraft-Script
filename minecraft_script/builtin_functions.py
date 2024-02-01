e7126af3afab7787b1d1e8b0e49135affab3c7a4
Bard-Gaming <christophe@dronne.fr>
2024-02-01 20:45:59 +0100

    Added builtin function repr

--- minecraft_script/builtin_functions.py
+++ minecraft_script/builtin_functions.py
@@ -1,4 +1,5 @@
 from .types import MCSFunction, MCSNull, MCSString, MCSNumber
+from .errors import MCSValueError
 
 
 def log(args, context) -> MCSNull:
@@ -8,8 +9,23 @@ def log(args, context) -> MCSNull:
 
 builtin_function_log = MCSFunction('log', None, None)
 builtin_function_log.call = log
-builtin_function_log.print_value = lambda: 'builtin-log'
+builtin_function_log.print_value = lambda: '<builtin-log>'
+
+
+def repr(args, context) -> MCSString:
+    if len(args) > 1:
+        raise MCSValueError(f"Function <builtin-repr> takes 1 argument, got {len(args)}")
+    value = args[0]
+
+    return MCSString(value.repr_value())
+
+
+builtin_function_repr = MCSFunction('repr', None, None)
+builtin_function_repr.call = repr
+builtin_function_repr.print_value = lambda: '<builtin-repr>'
+
 
 builtin_functions = [
-    builtin_function_log
+    builtin_function_log,
+    builtin_function_repr
 ]
