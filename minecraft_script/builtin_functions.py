86cca3adaa5dea5b5e7bcdc16a5cd1d9825bb48e
Bard-Gaming <christophe@dronne.fr>
2024-02-09 19:47:15 +0100

    Restructured builtin_functions.py

--- minecraft_script/builtin_functions.py
+++ minecraft_script/builtin_functions.py
@@ -1,46 +1,41 @@
 from .types import MCSFunction, MCSNull, MCSString, MCSNumber, MCSList
-from .errors import MCSValueError
+from .errors import MCSValueError, MCSTypeError
 
 
-def custom_log(args, context) -> MCSNull:
+def custom_log(args, context):
+    from .interpreter import RuntimeResult
+
     print(*[arg.print_value() for arg in args])
-    return MCSNull()
+    return RuntimeResult(return_value=MCSNull())
 
 
-builtin_function_log = MCSFunction('log', None, None)
-builtin_function_log.call = custom_log
-builtin_function_log.print_value = lambda: '<builtin-log>'
+def custom_repr(args, context):
+    from .interpreter import RuntimeResult
 
-
-def custom_repr(args, context) -> MCSString:
     if len(args) > 1:
         raise MCSValueError(f"Function <builtin-repr> takes 1 argument, got {len(args)}")
     value = args[0]
 
-    return MCSString(value.repr_value())
+    return RuntimeResult(return_value=MCSString(value.repr_value()))
 
 
-builtin_function_repr = MCSFunction('repr', None, None)
-builtin_function_repr.call = custom_repr
-builtin_function_repr.print_value = lambda: '<builtin-repr>'
+def custom_range(args, context):
+    from .interpreter import RuntimeResult
 
-
-def custom_range(args, context) -> MCSList:
     if len(args) > 1:
         raise MCSValueError(f"Function <builtin-range> takes 1 argument, got {len(args)}")
     range_length = args[0]
 
-    return MCSList(list(
+    if not isinstance(range_length, MCSNumber):
+        raise MCSTypeError(f"{range_length.class_name() !r} object cannot be interpreted as an integer")
+
+    return RuntimeResult(return_value=MCSList(list(
         map(lambda num: MCSNumber(num), range(range_length.get_value()))
-    ))
+    )))
 
-
-builtin_function_range = MCSFunction('range', None, None)
-builtin_function_range.call = custom_range
-builtin_function_range.print_value = lambda: '<builtin-range>'
 
 builtin_functions = [
-    builtin_function_log,
-    builtin_function_repr,
-    builtin_function_range,
+    custom_log,
+    custom_repr,
+    custom_range,
 ]
