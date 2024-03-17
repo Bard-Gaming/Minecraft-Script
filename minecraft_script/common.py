bb570ac7624aa67c3a93118deb3ceff602f68632
Bard-Gaming <christophe@dronne.fr>
2024-03-17 15:47:26 +0100

    removed redundant debuglogger from common file

--- minecraft_script/common.py
+++ minecraft_script/common.py
@@ -1,42 +1,8 @@
+from uuid import uuid4 as get_uuid
+
 version = "0.2.0"
 module_folder = "/".join(__file__.split('\\')[:-1])
 
 
-class DebugLogger:
-    global_indent_level = 0
-    indent_space = 2
-
-    def __new__(cls, *args, **kwargs):
-        self = object.__new__(cls)
-        return self.function_call
-
-    @classmethod
-    def indent_str(cls):
-        return " " * cls.global_indent_level * cls.indent_space
-
-    @classmethod
-    def function_call(cls, function):
-        def function_call_wrapper(*args, **kwargs):
-            print(f'{cls.indent_str()}Calling "{function.__name__}"...')
-
-            cls.global_indent_level += 1
-            output = function(*args, **kwargs)
-            print(f'{cls.indent_str()}"{function.__name__}" return: {output !r}') if output else None
-            cls.global_indent_level -= 1
-
-            print(f'{cls.indent_str()}Successfully called "{function.__name__}".')
-
-        return function_call_wrapper
-
-
-if __name__ == '__main__':
-    @DebugLogger()
-    def test():
-        bob()
-
-
-    @DebugLogger()
-    def bob():
-        return 2 + 2
-
-    test()
+def generate_uuid():
+    return str(get_uuid())
