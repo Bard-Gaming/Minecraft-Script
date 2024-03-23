28e45f3f55d750c7d3ae9b360c0b23959e7f05a6
Bard-Gaming <christophe@dronne.fr>
2024-03-23 19:11:12 +0100

    [Compiler] Added logical comparisons

---
+++ minecraft_script/build_templates/math/greater_equals_than.mcfunction
@@ -0,0 +1,2 @@
+scoreboard players set .out mcs_math 0
+execute if score .a mcs_math >= .b mcs_math run scoreboard players set .out mcs_math 1
--- minecraft_script/compiler/builtin_functions.py
+++ minecraft_script/compiler/builtin_functions.py
@@ -3,7 +3,7 @@ def log(interpreter, args) -> tuple[str]:
 
     values = list(map(lambda arg: (arg.get_storage(), arg.get_nbt()), args))
     values.extend(("", "none") for _ in range(max_length - len(args)))
-    
+
     storage = " {" + ", ".join(f'"s{i}": "{values[i][0]}", "n{i}": "{values[i][1]}"' for i in range(max_length)) + '}'
 
     return (f'function {interpreter.datapack_id}:builtins/log' + storage,)
