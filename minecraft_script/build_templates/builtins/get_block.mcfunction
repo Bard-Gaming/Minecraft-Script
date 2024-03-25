8397cff156736289f3a09ff25830343adf3c7564
Bard-Gaming <christophe@dronne.fr>
2024-03-25 14:04:56 +0100

    [Compiler] Finished get_block() function

--- minecraft_script/build_templates/builtins/get_block.mcfunction
+++
@@ -1,4 +0,0 @@
-summon armor_stand ~ ~5 ~ {Invisible:1b, NoBasePlate:1b, NoGravity:1b, Tags:["mcs_get_block_temp"]}
-$loot replace entity @e[type=armor_stand, limit=1, sort=nearest, tag=mcs_get_block_temp] armor.head mine $(x) $(y) $(z) netherite_pickaxe{Enchantments:[{id:"minecraft:silk_touch", lvl:1s}]}
-$data modify storage $(storage) string.$(nbt) set from entity @e[type=minecraft:armor_stand, limit=1] ArmorItems[3].id
-kill @e[type=armor_stand, tag=mcs_get_block_temp]
--- minecraft_script/compiler/builtin_functions.py
+++ minecraft_script/compiler/builtin_functions.py
@@ -29,24 +29,29 @@ def command(interpreter, args, context) -> tuple[tuple[str, ...], mcs_type]:
 
 
 def get_block(interpreter, args, context) -> tuple[tuple[str, ...], mcs_type]:
+    from .compile_interpreter import CompileContext
     x, y, z, *_ = args
 
-    value = MCSString(context)
+    local_context = CompileContext(f":cb_{generate_uuid()}", context)
 
-    # TODO: fix this method (generate it dynamically instead of having a fixed fnc?)
+    mcs_obj = MCSString(context)
 
-    commands = (
-        f"data modify storage mcs_{context.uuid} current.storage set value \"mcs_{context.uuid}\"",
-        f"execute store result storage mcs_{context.uuid} current.nbt int 1 run random value 10000000..99999999",
+    fnc_commands = (
+        "summon armor_stand ~ ~5 ~ {Invisible:1b, NoBasePlate:1b, NoGravity:1b, Tags:[\"mcs_get_block_temp\"]}",
+        "$loot replace entity @e[type=armor_stand, limit=1, sort=nearest, tag=mcs_get_block_temp] armor.head mine $(x) $(y) $(z) netherite_pickaxe{Enchantments:[{id:\"minecraft:silk_touch\", lvl:1s}]}",  # NOQA
+        f"data modify storage {mcs_obj.get_storage()} {mcs_obj.get_nbt()} set from entity @e[type=minecraft:armor_stand, limit=1] ArmorItems[3].id",  # NOQA
+        "kill @e[type=armor_stand, tag=mcs_get_block_temp]"
+    )
+    interpreter.add_commands(local_context.mcfunction_name, fnc_commands)
+
+    setup_commands = (
         f"data modify storage mcs_{context.uuid} current.x set from storage {x.get_storage()} {x.get_nbt()}",
         f"data modify storage mcs_{context.uuid} current.y set from storage {y.get_storage()} {y.get_nbt()}",
         f"data modify storage mcs_{context.uuid} current.z set from storage {z.get_storage()} {z.get_nbt()}",
-        f"function {interpreter.datapack_id}:builtins/get_block with storage mcs_{context.uuid} current",
-        # Current is now block id (str), so save its data in string value:
-        f"data modify storage {value.get_storage()} {value.get_nbt()} set from storage mcs_{context.uuid} current",
+        f"function {interpreter.datapack_id}:code_blocks/{local_context.mcfunction_name[1:]} with storage mcs_{context.uuid} current",
     )
 
-    return commands, value
+    return setup_commands, mcs_obj
 
 
 builtin_functions = (
