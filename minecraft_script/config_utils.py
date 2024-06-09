c04654148efb8903c92ce6cf944958f3cfc57311
Bard-Gaming <christophe@dronne.fr>
2024-06-09 15:56:02 +0200

    [Shell Commands] Added config options verbose and default_output_path

--- minecraft_script/config_utils.py
+++ minecraft_script/config_utils.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 from .common import COMMON_CONFIG, module_folder
 
 
@@ -7,9 +8,18 @@ def config_boolean_check(value: str, setting: str) -> bool:
 
     if py_value not in ('True', 'False'):
         print(f"Error: incorrect value {value !r} for setting {setting !r}")
-        exit()
+        exit(-1)
 
     return eval(py_value)  # safe to eval here
+
+
+def config_path_check(value: str, setting: str) -> str:
+    path = value.replace("\\", "/")
+    if not os.path.exists(path):
+        print(f"Error: path {value !r} doesn't exist for setting {setting !r}")
+        exit(-1)
+
+    return path
 
 
 def update_config(setting: str, value: str) -> None:
@@ -30,5 +40,7 @@ def update_config(setting: str, value: str) -> None:
 
 config_value_wrapper = {
     "pack_format": lambda x: x,  # don't check (it's just text anyway, it can be whatever)
-    "debug_comments": lambda x: config_boolean_check(x, "debug_comments")
+    "debug_comments": lambda x: config_boolean_check(x, "debug_comments"),
+    "verbose": lambda x: config_boolean_check(x, "verbose"),
+    "default_output_path": lambda x: config_path_check(x, "default_output_path"),
 }
